ef0f0e713e7ef4e8b9dd600fa233cb73e3f2c197
vagrant <vagrant@homestead>
2015-05-31 20:30:43 +0000

    init for dev

---
+++ Plugin.php
@@ -0,0 +1,37 @@
+<?php namespace Epic\Plugins;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Script\CommandEvent;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+	protected $composer;
+	protected $io;
+
+	public function activate(Composer $composer, IOInterface $io)
+	{
+		$this->composer = $composer;
+		$this->io = $io;
+	}
+
+	public static function getSubscribedEvents()
+	{
+		return array(
+			ScriptEvents::POST_INSTALL_CMD => 'postInstall',
+			ScriptEvents::POST_UPDATE_CMD => 'postInstall',
+		);
+	}
+
+	public function testEvent(Event $event)
+	{
+		echo '<pre>';
+		var_dump("here");
+		echo '</pre>';
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Init version Epic/Plugin for dev
