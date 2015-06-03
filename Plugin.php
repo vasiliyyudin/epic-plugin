7d98f14d157f39511a0c4b4a5e15e8eb7fc2856a
Vasily Yudin <v.yudin@bstd.ru>
2015-06-03 15:08:29 +0000

    precommit.

--- Plugin.php
+++ Plugin.php
@@ -28,10 +28,56 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 		);
 	}
 
+	public function printT($var){
+		var_dump($var);
+	}
+
 	public function testEvent(Event $event)
 	{
-		echo '<pre>';
-		var_dump("here");
-		echo '</pre>';
+//		foreach (get_declared_classes() as $class) {
+//			echo	$class . PHP_EOL;
+//		}
+
+		$dir = __DIR__ . '/../../../';
+		$this->printT($dir);
+
+		$allFiles = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir));
+		$phpFiles = new \RegexIterator($allFiles, '/\.php$/');
+
+		foreach ($phpFiles as $phpFile) {
+			/** @var \SplFileInfo $phpFile */
+//			$this->printT($phpFile);
+			if($phpFile->getBasename() != 'Staff.php'){
+				continue;
+			}
+
+			$content = file_get_contents($phpFile->getRealPath());
+			$tokens = token_get_all($content);
+
+			for ($index = 0; isset($tokens[$index]); $index++) {
+				if (!isset($tokens[$index][0])) {
+					continue;
+				}
+				if (T_INTERFACE === $tokens[$index][0] && $tokens[$index][1] == 'class') {
+					$index += 2; // Skip class keyword and whitespace
+
+					if(T_OBJECT_OPERATOR === $tokens[$index][0]){ // extends
+
+					}
+
+//					while (isset($tokens[$index]) && is_array($tokens[$index])) {
+//						$namespace .= $tokens[$index++][1];
+//					}
+				}
+//				if (T_CLASS === $tokens[$index][0]) {
+//					$index += 2; // Skip class keyword and whitespace
+//					$fqcns[] = $namespace.'\\'.$tokens[$index][1];
+//				}
+			}
+
+			var_dump($tokens);
+
+		}
+
 	}
 }
