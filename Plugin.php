530887f580647998c2eaaa833eb5efa4da5db609
vagrant <vagrant@homestead>
2015-06-08 19:45:08 +0000

    precommit

--- Plugin.php
+++ Plugin.php
@@ -34,29 +34,69 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 		$allFiles = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir));
 		$phpFiles = new \RegexIterator($allFiles, '/\.php$/');
 
+		$facadeClass = array();
 		foreach ($phpFiles as $phpFile) {
 			/** @var \SplFileInfo $phpFile */
-			if($phpFile->getBasename() != 'Staff.php'){
-				continue;
-			}
+//			if($phpFile->getBasename() != 'Staff.php'){
+//				continue;
+//			}
 
 			$content = file_get_contents($phpFile->getRealPath());
 			$tokens = token_get_all($content);
 
+			$classForExtend = false;
+			$namespace = '';
 			for ($index = 0; isset($tokens[$index]); $index++) {
-				if (!isset($tokens[$index][0])) {
-					continue;
+				if(is_array($tokens[$index]) && $tokens[$index][1] == 'namespace'){
+					$nmIndex = $index + 2;
+					do {
+						$namespace .= $tokens[$nmIndex][1];
+						$nmIndex++;
+					} while (is_array($tokens[$nmIndex]));
 				}
-				if (T_INTERFACE === $tokens[$index][0] && $tokens[$index][1] == 'class') {
-					$index += 2; // Skip class keyword and whitespace
+				if(!$classForExtend){
+					$classForExtend = $this->getUseClass($tokens, $index);
+					if($classForExtend){
+						continue;
+					}
+				}
 
-					if(T_OBJECT_OPERATOR === $tokens[$index][0]){ // extends
+				if(is_array($tokens[$index]) && $tokens[$index][1] == 'class' && is_array($tokens[$index + 4]) && $tokens[$index + 4][1] == 'extends'){
+					if(is_array($tokens[$index + 6]) && $tokens[$index + 6][1] == $classForExtend && is_array($tokens[$index + 2])){
+						$className = $tokens[$index + 2][1];
+						$facadeClass[] = [
+							'name' => $className,
+							'namespace' => $namespace,
+							'file' => $phpFile->getRealPath()
+						];
 
 					}
 				}
+
 			}
 
-			var_dump($tokens);
 		}
+
+		var_dump($facadeClass);
 	}
+
+	protected function getUseClass($tokens, $index){
+		$token = $tokens[$index];
+
+//		if(is_array($token) && $token[0] == T_GLOBAL && $token[1] == 'use'){ // T_GLOBAL = 346???
+		if(is_array($token)  && $token[1] == 'use'){
+//			$index += 2; // next index = whitespace
+			// use Epic\Facade\Facade;
+			if($tokens[$index + 2][1] == 'Epic' && $tokens[$index + 4][1] == 'Facade' && $tokens[$index + 6][1] == 'Facade'){
+				if($tokens[$index + 7] == ';'){
+					return 'Facade';
+				} else {
+					// use Epic\Facade\Facade as FF;
+					return $tokens[$index + 10][1];
+				}
+			}
+		}
+		return false;
+	}
+
 }
